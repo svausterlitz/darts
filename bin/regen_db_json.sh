623f481080c0b758d20c7ea30c5699fcb5300845
Bas Bloemsaat <bas@bloemsaat.com>
2020-02-01 19:40:13 +0100

    add update site to regen

--- bin/regen_db_json.sh
+++ bin/regen_db_json.sh
@@ -2,6 +2,9 @@
 
 export PATH=~/.pyenv/shims:~/.pyenv/bin:"$PATH"
 
+
+~/src/darts/bin/genSite2016-2017.pl --file data/Austerlitz_seizoen_2019-2020.xlsx --updatesite
+
 python ~/src/darts/bin/sva_darts_init.py
 python ~/src/darts/bin/sva_darts_calc.py
 python ~/src/darts/bin/sva_darts_gen_json.py
